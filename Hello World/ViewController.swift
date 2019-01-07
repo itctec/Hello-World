d09ced46fd1baf4cbe048a88a2df5facb4b0f63b
Itc <itctec@126.com>
2019-01-07 12:16:02 +0800

    UITextField测试

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //UILabel Test
-        let label=UILabel(frame: CGRect(x: 20, y: 100, width: 280, height: 30))
+        let label=UILabel(frame: CGRect(x: 10, y: 30, width: 280, height: 20))
         label.text="Hello Git!,I am yangwenjiang ,My wife is wangxi"
         label.backgroundColor=UIColor.red
-        label.font=UIFont.systemFont(ofSize: 23)
+        label.font=UIFont.systemFont(ofSize: 12)
         label.textColor=UIColor.white
         label.textAlignment = NSTextAlignment.center
         label.numberOfLines=0
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
         
         //UIButton
         let button=UIButton(type: UIButton.ButtonType.custom)
-        button.frame=CGRect(x: 40, y: 170, width: 120, height: 120)
+        button.frame=CGRect(x: 10, y: 60, width: 70, height: 70)
         button.backgroundColor=UIColor.blue
         //button.setBackgroundImage(UIImage(named:"ic_launcher"), for: UIControl.State.normal)
         button.setImage(UIImage(named: "ic_launcher"), for: UIControl.State.normal)
@@ -43,10 +43,42 @@ class ViewController: UIViewController {
         button.addTarget(self, action: #selector(changeColor), for: UIControl.Event.touchUpInside)
         self.view.addSubview(button)
         
+        //UITextField
+        let textField=UITextField(frame: CGRect(x: 90, y: 60, width: 250, height: 20))
+        textField.borderStyle=UITextField.BorderStyle.roundedRect
+        textField.placeholder="请输入文字"
+        textField.textColor=UIColor.blue
+        textField.font=UIFont.systemFont(ofSize: 12)
+        textField.textAlignment=NSTextAlignment.center
+        
+        let imageView=UIImageView(image: UIImage(named: "ic_launcher"))
+        imageView.frame=CGRect(x: 0, y: 0, width: 16, height: 16)
+        textField.leftView=imageView
+        textField.leftViewMode=UITextField.ViewMode.always
+        textField.delegate=self
+        self.view.addSubview(textField)
+        
     }
     
     @objc func changeColor(){
         self.view.backgroundColor=UIColor(red: CGFloat(arc4random()%255)/255.0, green: CGFloat(arc4random()%255)/255.0, blue: CGFloat(arc4random()%255)/255.0, alpha: 1)
+    }
+    
+    func textField(_ textField:UITextField,shouldChangeCharactersIn range:NSRange,replacementString string:String)-> Bool{
+        if string.count>0{
+            let charas:[Character]=["0","1","2","3","4","5","6","7","8","9"]
+            let char=string.first!
+            if !charas.contains(char){
+                print("请输入数字")
+                return false
+            }
+            
+            if textField.text!.count+string.count>11{
+                print("超过11位")
+                return false
+            }
+        }
+        return true
     }
 
 
