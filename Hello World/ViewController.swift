cc7a4b1631e9e8cf5bc09ac7db56303d1d8368a6
Itc <itctec@126.com>
2019-01-07 10:03:28 +0800

    UILabel 测试

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello Git!")
+        //UILabel Test
+        let label=UILabel(frame: CGRect(x: 20, y: 100, width: 280, height: 30))
+        label.text="Hello Git!,I am yangwenjiang ,My wife is wangxi"
+        label.backgroundColor=UIColor.red
+        label.font=UIFont.systemFont(ofSize: 23)
+        label.textColor=UIColor.white
+        label.textAlignment = NSTextAlignment.center
+        label.numberOfLines=0
+        label.lineBreakMode=NSLineBreakMode.byTruncatingMiddle
+        
+        self.view.addSubview(label)
+        
+        
+        
     }
 
 
